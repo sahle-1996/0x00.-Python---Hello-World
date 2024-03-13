dd4597229b8a0096e418a2bbe2dbfa580d6cb1ff
sahle-1996 <sahlesiumgoitom@gmail.com>
2024-03-13 12:47:34 -0700

    11-pow.py

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 def pow(a, b):
-    return a if b == 1 else a * pow(a, b - 1)
+    return exp_recursive(a, b)
+
+
+def exp_recursive(a, b):
+    if b == 0:
+        return 1
+    elif b < 0:
+        return 1 / (a * exp_recursive(a, -b - 1))
+    else:
+        return a * exp_recursive(a, b - 1)
