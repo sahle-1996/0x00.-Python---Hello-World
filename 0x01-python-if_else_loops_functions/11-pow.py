3985f0fcafab376a8ab68b305310643262264e53
sahle-1996 <sahlesiumgoitom@gmail.com>
2024-03-13 12:43:27 -0700

    11-pow.py

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    result = 1
-    for _ in range(abs(b)):
-        result *= a if b > 0 else 1 / a
-    return result
+    return a if b == 1 else a * pow(a, b - 1)
