a0f590b619bb68ac181f374c63c0ced5dc78370b
sahle-1996 <sahlesiumgoitom@gmail.com>
2024-03-20 08:36:08 -0700

    1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+
 if __name__ == "__main__":
-    import calculator_1
-    a = 10
-    b = 5
-    add = calculator_1.add
-    sub = calculator_1.sub
-    mul = calculator_1.mul
-    div = calculator_1.div
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
+    a, b = 10, 5
+    expressions = [
+        ("{} + {} = {}", add(a, b)),
+        ("{} - {} = {}", sub(a, b)),
+        ("{} * {} = {}", mul(a, b)),
+        ("{} / {} = {}", div(a, b))
+    ]
+    
+    for expr in expressions:
+        print(expr[0].format(a, b, expr[1]))
