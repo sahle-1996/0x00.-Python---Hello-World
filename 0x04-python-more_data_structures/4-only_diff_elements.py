d1e3fbf6c2ebea3e7fa1294e3fe773a27914cdc7
sahle-1996 <sahlesiumgoitom@gmail.com>
2024-03-22 11:06:19 -0700

    4-only_diff_elements.py

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
 
-from typing import Set
-
-def only_diff_elements(set_1: Set[str], set_2: Set[str]) -> Set[str]:
-    """Returns a set containing elements present in only one of the two input sets."""
-    return set_1.symmetric_difference(set_2)
-
-set_1 = { "Python", "C", "Javascript" }
-set_2 = { "Bash", "C", "Ruby", "Perl" }
-print(sorted(list(only_diff_elements(set_1, set_2))))
+def only_diff_elements(set_1, set_2):
+    return set(set_1).symmetric_difference(set_2)
