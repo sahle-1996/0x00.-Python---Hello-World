99dc1c52892fc06907751786c8aa382e67eeb17d
sahle-1996 <sahlesiumgoitom@gmail.com>
2024-05-23 06:11:29 -0700

    0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,5 @@
--- Script to list all privileges of the MySQL users user_0d_1 and user_0d_2
-SHOW GRANTS;
+-- Lists all privileges of the users user_0d_1 and user_0d_2
+SELECT grantee, privilege_type 
+FROM information_schema.user_privileges 
+WHERE grantee = "'user_0d_1'@'localhost'"
+OR grantee = "'user_0d_2'@'localhost'";
