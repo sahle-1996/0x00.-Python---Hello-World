70f804cfdd03fc9f360d671a354ded210d1d254e
sahle-1996 <sahlesiumgoitom@gmail.com>
2024-05-23 06:45:37 -0700

    13-count_shows_by_genre.sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,2 @@
--- Script to list all genres from hbtn_0d_tvshows and display the number of shows linked to each
-SELECT tg.name AS genre,
-       COUNT(tsg.show_id) AS number_of_shows
-FROM tv_genres tg
-LEFT JOIN tv_show_genres tsg ON tg.id = tsg.genre_id
-GROUP BY tg.id
-ORDER BY number_of_shows DESC,
-         tg.id ASC;
+-- Script to remove entries from the table second_table where score is less than or equal to 5.
+DELETE FROM second_table WHERE score <= 5;
