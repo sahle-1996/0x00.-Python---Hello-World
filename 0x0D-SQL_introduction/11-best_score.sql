9848ce12587b8181a27e904e7a6aec3704373578
sahle-1996 <sahlesiumgoitom@gmail.com>
2024-05-23 05:56:22 -0700

    11-best_score.sql

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,5 @@
--- Command to display all records with a score greater than or equal to 10 from the table second_table in the database hbtn_0c_0
-SELECT name, score FROM second_table WHERE score >= 10 ORDER BY score DESC;
+-- Prints the sorted database, filtering records with a score greater than or equal to 10
+SELECT score, name 
+FROM second_table 
+WHERE score >= 10 
+ORDER BY 1 DESC, 2;
