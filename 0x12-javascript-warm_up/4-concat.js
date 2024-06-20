370ee878af34a317fd7346443ae834b08d5bdb0d
sahle-1996 <sahlesiumgoitom@gmail.com>
2024-06-20 13:36:45 -0700

    4-concat.js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,3 +1,5 @@
 #!/usr/bin/node
-const [,, firstArg, secondArg] = process.argv;
-console.log(firstArg ? `${firstArg} is ${secondArg}` : 'Missing arguments');
+
+const args = require('process').argv.slice(2);
+
+console.log(args[0] ? `${args[0]} is ${args[1] || 'undefined'}` : 'Insufficient arguments');
