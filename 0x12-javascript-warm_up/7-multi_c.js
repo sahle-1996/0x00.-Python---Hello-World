9d76811eaec8a35d2db75255b0c7dc72cb8f5420
sahle-1996 <sahlesiumgoitom@gmail.com>
2024-06-20 14:18:49 -0700

    7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const count = parseInt(process.argv[2]);
-const repeatMessage = (times) => {
-  for (let i = 0; i < times; i++) console.log('C is fun');
-};
-
-isNaN(count) ? console.log('Missing number of occurrences') : repeatMessage(count);
+const n = parseInt(process.argv[2]);
+if (Number.isNaN(n)) {
+  console.log('Missing number of occurrences');
+} else {
+  Array.from({ length: n }).forEach(() => console.log('C is fun'));
+}
