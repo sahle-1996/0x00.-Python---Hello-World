0da921c1cbe576e51ed1aa2a5c49600852587d87
sahle-1996 <sahlesiumgoitom@gmail.com>
2024-06-20 15:28:24 -0700

    1-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  area() {
+    return this.width * this.height;
+  }
+
+  perimeter() {
+    return 2 * (this.width + this.height);
+  }
+}
+
+module.exports = Rectangle;
