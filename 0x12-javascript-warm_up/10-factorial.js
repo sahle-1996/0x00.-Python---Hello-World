5da81b0ae7fc8b88739ab009d4f8ff84c1ad9d36
sahle-1996 <sahlesiumgoitom@gmail.com>
2024-06-20 14:54:34 -0700

    10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const fac = (num) => (num <= 1 || isNaN(num)) ? 1 : num * fac(num - 1);
+const getFactorial = (n) => n > 1 ? n * getFactorial(n - 1) : 1;
 
-const num = parseInt(process.argv[2]);
+const number = parseInt(process.argv[2]);
 
-console.log(fac(num));
+console.log(isNaN(number) ? 1 : getFactorial(number));
