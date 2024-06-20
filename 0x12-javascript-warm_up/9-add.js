91afd64d843eaf05513d8d93c055ae0e16ff9dbf
sahle-1996 <sahlesiumgoitom@gmail.com>
2024-06-20 14:36:43 -0700

    9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 
-const sum = (x, y) => x + y;
+const sum = (a, b) => a + b;
 
-const a = parseInt(process.argv[2]);
-const b = parseInt(process.argv[3]);
+const args = process.argv.slice(2).map(Number);
 
-console.log(sum(a, b));
+console.log(sum(...args));
