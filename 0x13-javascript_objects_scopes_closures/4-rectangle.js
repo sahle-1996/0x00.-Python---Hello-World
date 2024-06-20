f040ab7bdc996603f94c4eabe0710785f112179e
sahle-1996 <sahlesiumgoitom@gmail.com>
2024-06-20 16:10:06 -0700

    4-rectangle.js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,2 +1,25 @@
 #!/usr/bin/node
-console.log(`${process.argv[2]} is ${process.argv[3]}`);
+
+class Rectangle {
+  constructor(w, h) {
+    this.width = w > 0 ? w : 0;
+    this.height = h > 0 ? h : 0;
+  }
+
+  print() {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate() {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double() {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
