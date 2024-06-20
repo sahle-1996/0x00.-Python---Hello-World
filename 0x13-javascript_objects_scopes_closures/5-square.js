8972923e61de02b9f08d4bab8bae61cf3f9c8005
sahle-1996 <sahlesiumgoitom@gmail.com>
2024-06-20 16:18:09 -0700

    5-square.js

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,4 +1,16 @@
 #!/usr/bin/node
 
-const number = parseInt(process.argv[2]);
-console.log(Number.isNaN(number) ? 'Not a number' : `My number: ${number}`);
+class Rectangle {
+  constructor(w, h) {
+    this.width = w > 0 ? w : 0;
+    this.height = h > 0 ? h : 0;
+  }
+}
+
+class Square extends Rectangle {
+  constructor(size) {
+    super(size, size);
+  }
+}
+
+module.exports = Square;
