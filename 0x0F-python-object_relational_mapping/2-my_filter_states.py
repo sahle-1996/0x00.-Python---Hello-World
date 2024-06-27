318355124b690bcbf61136630b20f8caace13b73
sahle-1996 <sahlesiumgoitom@gmail.com>
2024-06-27 01:31:16 -0700

    my_filter_states

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,30 +1,30 @@
 #!/usr/bin/python3
 """
 Script that takes in an argument and displays all values in the states
-table of hbtn_0e_0_usa where name matches the argument
+table of hbtn_0e_0_usa where name matches the argument.
 """
-import MySQLdb
-from sys import argv
 
-# The code should not be executed when imported
+import MySQLdb
+import sys
+
 if __name__ == '__main__':
     # Make a connection to the database
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
 
     # Create a cursor object to execute queries
-    cur = db.cursor()
+    cursor = db.cursor()
 
-    # Execute SQL query with user input for state name
-    state_name = argv[4]
-    query = "SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id ASC"
-    cur.execute(query, (state_name,))
+    # Define the SQL query with user input for state name
+    state_name = sys.argv[4]
+    sql_query = "SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id ASC"
+    cursor.execute(sql_query, (state_name,))
 
     # Fetch and print the result row if exists
-    row = cur.fetchone()
+    row = cursor.fetchone()
     if row:
         print(row)
 
     # Clean up process
-    cur.close()
+    cursor.close()
     db.close()
