4f607194058f44a99564811ed6153eb9f5a7fd57
sahle-1996 <sahlesiumgoitom@gmail.com>
2024-07-04 12:09:42 -0700

    body

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 # GET method using curl and display body of a 200 status code response
-
-# Send GET request and capture both output and HTTP status
-response=$(curl -sL -w "%{http_code}" "$1")
-
-# Extract the HTTP status code
-status_code="${response: -3}"
-
-# Display body only if status code is 200
-[ "$status_code" == "200" ] && curl -sL "$1"
+curl -sL "$1"
