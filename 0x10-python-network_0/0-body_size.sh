798ddb206d90aeedac8b6c888184403fb7d17eb4
sahle-1996 <sahlesiumgoitom@gmail.com>
2024-07-04 11:48:20 -0700

    body_size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,11 +1,3 @@
 #!/usr/bin/env bash
-# Script to fetch the URL and display the size of the response body in bytes
-
-response=$(curl -s -D - "$1" -o /dev/null)
-content_length=$(echo "$response" | grep -i "Content-Length" | awk '{print $2}')
-
-if [ -n "$content_length" ]; then
-    echo "$content_length"
-else
-    curl -s "$1" | wc -c
-fi
+# Retrieve URL content with curl, count the bytes in the response body
+curl -s "$1" | { read -r body; echo "${#body}"; }
