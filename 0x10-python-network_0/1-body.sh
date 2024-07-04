6f94ddce5950840df3a22a3f8869132bafa98c47
sahle-1996 <sahlesiumgoitom@gmail.com>
2024-07-04 12:01:46 -0700

    body

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-# Sends a GET request to a URL with curl and displays the body of the response (200 status code only)
-
-response=$(curl -sL -w "%{http_code}" "$1")
-
-# Check if the response code is 200
-if [[ $response == *"200"* ]]; then
-    curl -sL "$1" | grep -o 'Rout 2'
-fi
+# Fetch URL content with curl, output body only if status code is 200
+curl -s -o /dev/null -w "%{http_code}" "$1" | grep -q "^200$" && curl -s "$1"
