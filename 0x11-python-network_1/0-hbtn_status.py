4b2d70496f93c38d836c9904561fefdc173c4aa3
sahle-1996 <sahlesiumgoitom@gmail.com>
2024-07-05 06:45:20 -0700

    hbtn_status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Fetches data from
+    https://alx-intranet.hbtn.io/status
+"""
+
+import urllib.request
+
+url = "https://alx-intranet.hbtn.io/status"
+with urllib.request.urlopen(url) as response:
+    data = response.read()
+    print("Body response:")
+    print("\t- type:", type(data))
+    print("\t- content:", data)
+    print("\t- utf8 content:", data.decode('utf-8'))
